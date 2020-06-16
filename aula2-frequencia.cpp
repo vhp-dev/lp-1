2736f30d0e607eb7a0c66f060a1c60880ffaf716
Vitor Hugo Pinto <vitorhugop.dev@gmail.com>
2020-06-16 17:30:02 -0300

    correcao da excecao do add

--- aula2-frequencia.cpp
+++ aula2-frequencia.cpp
@@ -5,21 +5,22 @@ int main(int argc, char *argv[])
 {
     std::string mensagem;
     
-    if (argc == 1)
+    if (argc == 1 || strcmp(argv[1],"add"))
     {
         std::cout << "Uso: " << argv[0] << " add <mensagem>" << std::endl;
         return -1;
     }
     
-    if (!strcmp(argv[1],"add") && argc < 3)
+    if (!strcmp(argv[1],"add"))
     {
-        std::cout << "Digite uma mensagem: ";
-        getline(std::cin, mensagem);
-        std::cout << mensagem << std::endl;
-    }
-    else
-    {
-        std::cout <<  argv[2] << std::endl;
+        if ( argc < 3)
+        {
+            std::cout << "Digite uma mensagem: ";
+            getline(std::cin, mensagem);
+            std::cout << mensagem << std::endl;
+        }
+        else
+            std::cout <<  argv[2] << std::endl;
     }
 
     return 0;
